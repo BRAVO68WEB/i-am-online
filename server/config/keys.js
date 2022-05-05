43e10ed73830fea355fe7019d72302033824c0ac
BRAVO68WEB <jbandyopadhayaya@gmail.com>
2022-05-05 14:02:29 +0530

    restructure service api

--- server/config/keys.js
+++ server/config/keys.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 module.exports = {
   mongoURI: process.env.MONGO_URI,
   sessionSecret: process.env.SESSION_SECRET,
-  discordId: process.env.DISCORD_ID,
   pingAuthKey: process.env.PING_REG_AUTH_KEY,
   pingusername: process.env.USERNAME_TO_PING,
 };
